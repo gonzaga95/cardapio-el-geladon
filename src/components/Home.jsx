c71d01da1153d4fe2acb60129e94926351bb269f
Carlos Eduardo Gonzaga <91717487+gonzaga95@users.noreply.github.com>
2023-01-09 21:31:07 -0400

    remove comentarios no index e format home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,13 @@ import "./Home.css";
 import PaletaLista from "PaletaLista";
 
 function Home() {
-  return (
-    <div className="Home">
-      <div className="Home__container">
-        <PaletaLista />
-      </div>
-    </div>
-  );
+    return (
+        <div className="Home">
+            <div className="Home__container">
+                <PaletaLista />
+            </div>
+        </div>
+    );
 }
 
 export default Home;
