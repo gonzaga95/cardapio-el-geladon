ae6fc8a73682c6b176751f6c5daac4c761ebf7fb
Carlos Eduardo Gonzaga <91717487+gonzaga95@users.noreply.github.com>
2023-01-09 20:15:38 -0400

    add index home and images

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-ReactDOM.render(
-  <React.StrictMode>Primeiro projeto. Iniciando os commits. Módulo 04 do curso de FullStack da Blue EdTech</React.StrictMode>,
-  document.getElementById("root")
-);
